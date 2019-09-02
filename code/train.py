5bbb60d2b3251a94b3a8804701634c72a19e1d3b
Michael Wellner <michael.wellner@de.ibm.com>
2019-09-02 13:00:38 +0200

    Added MLFlow.

--- code/train.py
+++ code/train.py
@@ -13,6 +13,11 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.metrics import accuracy_score
 
+import mlflow
+import mlflow.sklearn
+
+mlflow.set_tracking_uri('../mlruns')
+
 # Result Dictionary
 results = {}
 
@@ -22,26 +27,37 @@ x_test = pd.read_pickle('../data/test_prepared.pkl')
 x_train = train.drop(['Survived'], axis = 1)
 y_train = train['Survived']
 
-# Logistic Regression
-logreg = LogisticRegression(solver='lbfgs', max_iter = 1000)
-logreg.fit(x_train, y_train)
-y_pred = logreg.predict(x_test)
-acc_log = round(logreg.score(x_train, y_train) * 100, 2)
-results['Logistic Regression (ACC)'] = acc_log
+with mlflow.start_run():
+    max_iter = 1000 # Might passed from command line arg....
 
-# Support Vector Machines
-svc = SVC(gamma='auto')
-svc.fit(x_train, y_train)
-y_pred = svc.predict(x_test)
-acc_svc = round(svc.score(x_train, y_train) * 100, 2)
-results['Support Vector Machines (ACC)'] = acc_svc
+    mlflow.log_param('lr.max_iter', max_iter)
+    mlflow.log_param('some-other-value', 'foo bar')
 
-# Decision Tree
-decision_tree = DecisionTreeClassifier()
-decision_tree.fit(x_train, y_train)
-y_pred = decision_tree.predict(x_test)
-acc_decision_tree = round(decision_tree.score(x_train, y_train) * 100, 2)
-results['Decission Tree (ACC)'] = acc_decision_tree
+    # Logistic Regression
+    logreg = LogisticRegression(solver='lbfgs', max_iter = max_iter)
+    logreg.fit(x_train, y_train)
+    y_pred = logreg.predict(x_test)
+    acc_log = round(logreg.score(x_train, y_train) * 100, 2)
+    mlflow.sklearn.log_model(logreg, "model")
 
-with open('../data/metrics.json', 'w') as fp:
-    json.dump(results, fp, indent=2, sort_keys=True)
+    results['Logistic Regression (ACC)'] = acc_log
+    mlflow.log_metric('lr.acc', acc_log)
+
+    # Support Vector Machines
+    svc = SVC(gamma='auto')
+    svc.fit(x_train, y_train)
+    y_pred = svc.predict(x_test)
+    acc_svc = round(svc.score(x_train, y_train) * 100, 2)
+    results['Support Vector Machines (ACC)'] = acc_svc
+    mlflow.log_metric('svm.acc', acc_svc)
+
+    # Decision Tree
+    decision_tree = DecisionTreeClassifier()
+    decision_tree.fit(x_train, y_train)
+    y_pred = decision_tree.predict(x_test)
+    acc_decision_tree = round(decision_tree.score(x_train, y_train) * 100, 2)
+    results['Decission Tree (ACC)'] = acc_decision_tree
+    mlflow.log_metric('dt.acc', acc_decision_tree)
+
+    with open('../data/metrics.json', 'w') as fp:
+        json.dump(results, fp, indent=2, sort_keys=True)
